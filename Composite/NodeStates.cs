1844bf9fc0938498db76ce43f0b058ceb417c9ef
akka207 <alex2007kosh@gmail.com>
2026-05-09 13:43:40 +0200

    Complete state

---
+++ Composite/NodeStates.cs
@@ -0,0 +1,22 @@
+﻿namespace HTMLCreator;
+
+public interface INodeState
+{
+    string RenderState(LightElementNode node);
+}
+
+public class VisibleState : INodeState
+{
+    public string RenderState(LightElementNode node)
+    {
+        return node.GetOriginalOuterHtml();
+    }
+}
+
+public class HiddenState : INodeState
+{
+    public string RenderState(LightElementNode node)
+    {
+        return $"<!-- Прихований елемент: {node.TagName} -->";
+    }
+}
